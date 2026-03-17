995a17812b37572c0fda266cc722ea83f2075c74
Juan Sanabria <jupsanabriav@hotmail.com>
2026-03-16 22:12:14 -0500

    ajustes db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 async function connectDb() {
-  const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/proyecto-final-lite';
+  const mongoUri =
+    process.env.MONGOBD;
   await mongoose.connect(mongoUri);
 }
 
